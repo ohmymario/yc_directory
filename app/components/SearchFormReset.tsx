3fb7057c24680d9d41ec7d4c7d07a0dd5d80ddf3
Mario Ballesteros <mariob360@gmail.com>
2025-08-06 15:17:36 -0700

    refactor(SearchFormReset): comment out unused code and update link structure
    - Comment out the useRef and handleReset function for potential future use
    - Update Link component to use the X icon from lucide-react for better visual representation

--- app/components/SearchFormReset.tsx
+++ app/components/SearchFormReset.tsx
@@ -1,23 +1,29 @@
 'use client';
 
 import Link from 'next/link';
-import { useRef } from 'react';
+// import { useRef } from 'react';
+import { X } from 'lucide-react';
 
 const SearchFormReset = () => {
-  const ref = useRef<HTMLAnchorElement>(null);
+  // const ref = useRef<HTMLAnchorElement>(null);
 
-  const handleReset = () => {
-    const form = ref.current?.closest('form') as HTMLFormElement;
+  // const handleReset = () => {
+  //   const form = ref.current?.closest('form') as HTMLFormElement;
 
-    if (form) {
-      form.reset();
-    }
-  };
+  //   if (form) {
+  //     form.reset();
+  //   }
+  // };
 
   return (
     <>
-      <Link href='/' ref={ref} className='search-btn text-white' onClick={handleReset}>
-        X
+      <Link
+        href='/'
+        // ref={ref}
+        className='search-btn text-white'
+        // onClick={handleReset}
+      >
+        <X className='size-5' />
       </Link>
     </>
   );
